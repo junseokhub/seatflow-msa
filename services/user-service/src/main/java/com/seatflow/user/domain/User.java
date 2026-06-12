46129d3c4a29be059efea21c23dde51f2061ffaa
junseokhub <github.yam117@passinbox.com>
2026-06-12 16:02:55 +0900

    feat: Add user-service User Entity

---
+++ services/user-service/src/main/java/com/seatflow/user/domain/User.java
@@ -0,0 +1,36 @@
+package com.seatflow.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column()
+    private String phone;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
