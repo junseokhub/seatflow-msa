8a9632bc153a4cbdfa74533b8d06af5b09c721ed
junseokhub <github.yam117@passinbox.com>
2026-06-17 19:18:32 +0900

    fix: remove OutboxJpaConfiguration to resolve JPA repository scan conflict

--- libs/common-outbox/src/main/java/com/seatflow/common/outbox/CommonOutboxAutoConfiguration.java
+++ libs/common-outbox/src/main/java/com/seatflow/common/outbox/CommonOutboxAutoConfiguration.java
@@ -2,10 +2,8 @@ package com.seatflow.common.outbox;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @AutoConfiguration
-@EnableScheduling
-@Import({OutboxJpaConfiguration.class, OutboxStore.class, OutboxPublisher.class})
+@Import({OutboxStore.class, OutboxPublisher.class})
 public class CommonOutboxAutoConfiguration {
 }
